397e0d0475bfc5e0fa41bd3045986f60c38c715c
neurolit <neurolit@gmail.com>
2013-05-27 21:05:39 +0200

    Changements dans Grunt : * Nouvelle tâche meta_json_pretty_print pour avoir un meta.json bien indenté * Renommage de la tâche travis en test

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,9 +20,14 @@ module.exports = function (grunt) {
     });
 
     // default task.
-    grunt.registerTask('default', 'jslint');
+    grunt.registerTask('default', ['jslint', 'meta_json_pretty_print']);
 
     // Travis CI task.
-    grunt.registerTask('travis', 'jslint');
+    grunt.registerTask('test', 'jslint');
+
+    grunt.registerTask('meta_json_pretty_print', function() {
+        var metadata = grunt.file.readJSON('meta.json') ;
+        grunt.file.write('meta.json',JSON.stringify(metadata, null, 4));
+    })
 
 };
